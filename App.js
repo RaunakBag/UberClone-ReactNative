4da53efff2e7219ad59542867fd88528c5037f96
RaunakBag <raunakbag97@gmail.com>
2021-08-07 20:19:58 +0530

    Started

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import {Provider} from 'react-redux'
 import {store} from './app/redux/store'
 import HomeScreen from './app/screens/HomeScreen';
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView  } from 'react-native'
 
 const HomeScreen = () => {
     return (
         <SafeAreaView>
-                <Text>Hello</Text>
+                <Text style = {styles.text}>Hello!</Text>
         </SafeAreaView>
     )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    text: {
+        color: "blue",
+    },
+})
