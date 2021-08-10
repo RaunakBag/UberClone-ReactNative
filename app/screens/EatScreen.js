d03d0b7caddcd2c4e626330a8f883b0e76022ff2
RaunakBag <raunakbag97@gmail.com>
2021-08-10 20:20:53 +0530

    Working

---
+++ app/screens/EatScreen.js
@@ -0,0 +1,15 @@
+  
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const EatScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default EatScreen;
+
+const styles = StyleSheet.create({});
